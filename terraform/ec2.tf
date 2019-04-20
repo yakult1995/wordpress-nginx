450558595f9bd251ceeb2aaa503c62cfaa8079cc
yakult1995 <destin.tax@outlook.jp>
2019-04-20 15:50:12 -0700

    :bug: Fix terraform & packer

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_key_pair" "wp-key" {
 }
 
 resource "aws_instance" "wp-ec2" {
-    ami = "ami-0de53d8956e8dcf80" # 好きなAMIを選択
+    ami = "${var.ec2-ami}"
     instance_type = "t2.micro"
     key_name = "${var.key_name}"
     subnet_id = "${aws_subnet.public.id}"
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -25,9 +25,15 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.main.id}"
 }
 
-# Route the public subnet trafic through the IGW
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.main.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.gw.id}"
+resource "aws_route_table" "route_table" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.route_table.id}"
 }
